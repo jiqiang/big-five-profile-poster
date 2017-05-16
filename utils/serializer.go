17b48be40622c3f90dfdaf4253c328e496d2bd4c
Glenn Ji <qiang.ji@yahoo.com.au>
2017-05-16 22:55:40 +1000

    add read and hash method receivers.

--- utils/serializer.go
+++ utils/serializer.go
@@ -1,4 +1,16 @@
 package utils
 
 // BigFiveResultsTextSerializer class
-type BigFiveResultsTextSerializer struct{}
+type BigFiveResultsTextSerializer struct {
+	profile string
+}
+
+// Read function
+func (s *BigFiveResultsTextSerializer) Read(text string) {
+	s.profile = text
+}
+
+// Hash function
+func (s *BigFiveResultsTextSerializer) Hash() string {
+	return s.profile
+}
